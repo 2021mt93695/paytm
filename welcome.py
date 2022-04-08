1267b520e747ba332f460655d0ffa12f7ebc83da
BhaskarM_2021mt93695 <103062446+2021mt93695@users.noreply.github.com>
2022-04-08 19:24:07 +0530

    2021mt93695 updated CR1A

--- welcome.py
+++ welcome.py
@@ -1,9 +1,10 @@
 print("Hello world")
 print("dev feature A")
 print("dev feature A bugfixes")
-cr = True
-if (cr):
+cr1A = True
+if (cr1A):
     print("dev feature A CR1")
 
 print("2021mt93695 feature B")
 print("2021mt93695 feature B bugfixes")
+
