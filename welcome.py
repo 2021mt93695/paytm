775bc9909ffc72a62bcc080718d9662df974bcd4
bhaskarmenugula <bhaskarmenugula@gmail.com>
2022-04-08 19:28:46 +0530

    Bhaskar code changes CR1A

--- welcome.py
+++ welcome.py
@@ -4,6 +4,6 @@ print("dev feature A bugfixes")
 cr = True
 if (cr):
     print("dev feature A CR1")
-
+    print("code changes in CR1")
 print("2021mt93695 feature B")
 print("2021mt93695 feature B bugfixes")
